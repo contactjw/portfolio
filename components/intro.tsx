1579a5fbd8dd0d09095bc0cdc7e807b557d582f3
John West <contactjohnwest@gmail.com>
2023-12-22 22:33:53 -0800

    add profile photo and emoji elements to intro component

---
+++ components/intro.tsx
@@ -0,0 +1,43 @@
+'use client';
+
+import Image from 'next/image';
+import React from 'react';
+import profile_pic from '@/public/profilepic.png';
+import { motion } from 'framer-motion';
+
+export default function Intro() {
+  return (
+    <section>
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ type: 'tween', duration: 0.2 }}
+          >
+            <Image
+              src={profile_pic}
+              alt="Photo of John West"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 left-0 text-4xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+    </section>
+  );
+}
