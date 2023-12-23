efaa9bbefcb3576a4cae7bbd5b4d4e076a24df62
John West <contactjohnwest@gmail.com>
2023-12-23 13:49:05 -0800

    add developer description and contact links/buttons

--- components/intro.tsx
+++ components/intro.tsx
@@ -4,10 +4,14 @@ import Image from 'next/image';
 import React from 'react';
 import profile_pic from '@/public/profilepic.png';
 import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { HiDownload } from 'react-icons/hi';
+import { FaGithubSquare } from 'react-icons/fa';
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -38,6 +42,60 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+      <motion.p
+        className="mb-10 mt-5 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">{`Hello, I'm John. `}</span>
+        {`I'm a`} <span className="font-bold">full-stack developer</span> and{' '}
+        <span className="font-bold">student</span>. I enjoy building{' '}
+        <span className="italic">sites & apps</span>. My focus is{' '}
+        <span className="underline">React (Next.js) and Vue.js</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-3 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{' '}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+          href="/John_West_Resume.pdf"
+          download={true}
+        >
+          Download CV{' '}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <div className="flex items-center justify-center gap-3">
+          <a
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-3 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+            href="https://www.linkedin.com/in/johnjw1/"
+            target="_blank"
+          >
+            <BsLinkedin />
+          </a>
+
+          <a
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-3 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer border border-black/10"
+            href="https://github.com/contactjw"
+            target="_blank"
+          >
+            <FaGithubSquare />
+          </a>
+        </div>
+      </motion.div>
     </section>
   );
 }
