389dc3eccfe3c205b1961df959c928880264465c
John West <contactjohnwest@gmail.com>
2024-01-13 11:02:04 -0800

    add dark theme classes

--- components/experience.tsx
+++ components/experience.tsx
@@ -9,10 +9,11 @@ import {
 import 'react-vertical-timeline-component/style.min.css';
 import { experiencesData } from '@/lib/data';
 import { useSectionInView } from '@/lib/hooks';
+import { useTheme } from '@/context/theme-context';
 
 export default function Experience() {
   const { ref } = useSectionInView('Experience');
-
+  const { theme } = useTheme();
   return (
     <section id="experience" ref={ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
@@ -21,19 +22,24 @@ export default function Experience() {
           <React.Fragment key={index}>
             <VerticalTimelineElement
               contentStyle={{
-                background: '#f3f4f6',
+                background:
+                  theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
                 boxShadow: 'none',
                 border: '1px solid rgba(0, 0, 0, 0.05)',
                 textAlign: 'left',
                 padding: '1.3rem 2rem',
               }}
               contentArrowStyle={{
-                borderRight: '0.4rem solid #9ca3af',
+                borderRight:
+                  theme === 'light'
+                    ? '0.4rem solid #9ca3af'
+                    : '0.4rem solid rgba(255, 255, 255, 0.5)',
               }}
               date={experienceItem.date}
               icon={experienceItem.icon}
               iconStyle={{
-                background: 'white',
+                background:
+                  theme === 'light' ? 'white' : 'rgba(255, 255, 255, 0.15)',
                 fontSize: '1.5rem',
               }}
             >
@@ -41,7 +47,7 @@ export default function Experience() {
                 {experienceItem.title}
               </h3>
               <p className="font-normal !mt-0">{experienceItem.location}</p>
-              <p className="!mt-1 !font-normal text-gray-700">
+              <p className="!mt-1 !font-normal text-gray-700 dark:text-white/75">
                 {experienceItem.description}
               </p>
             </VerticalTimelineElement>
--- components/intro.tsx
+++ components/intro.tsx
@@ -82,7 +82,7 @@ export default function Intro() {
           <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
         <a
-          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
           href="/John_West_Resume.pdf"
           download={true}
         >
@@ -92,14 +92,14 @@ export default function Intro() {
 
         <div className="flex items-center justify-center gap-3">
           <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-3 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack"
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-3 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://www.linkedin.com/in/johnjw1/"
             target="_blank"
           >
             <BsLinkedin />
           </a>
           <a
-            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-3 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer borderBlack"
+            className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-3 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15]  active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
             href="https://github.com/contactjw"
             target="_blank"
           >
